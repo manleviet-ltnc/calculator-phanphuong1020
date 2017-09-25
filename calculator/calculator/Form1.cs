a86541ad9e12307366f2b035f21437761febf054
phanphuong1020 <phanthiphuong1020@gmail.com>
2017-09-25 11:16:53 +0700

    nhập số

---
+++ calculator/calculator/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class v : Form
+    {
+        public v()
+        {
+            InitializeComponent();
+        }
+        bool isTypinfNumber = false;
+        private void NhapSo(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            NhapSo(btn.Text);
+        }
+        private void NhapSo(string so)
+        {
+            if (isTypinfNumber)
+                lblDisplay.Text = lblDisplay.Text + so;
+            else
+            {
+                lblDisplay.Text = so;
+                isTypinfNumber = true;
+            }
+        }
+    }
+}
